8dedf003892c689f491f5cbcffc9dc9f6e7d0646
Rifqi Maulana <abelcaster025@gmail.com>
2024-03-13 15:36:47 +0700

    add queries in all model file

--- app/models/Penulis_model.php
+++ app/models/Penulis_model.php
@@ -12,21 +12,50 @@ class Penulis_model
 
   public function getAllPenulis()
   {
+    $this->db->query('SELECT * FROM ' . $this->table);
+    return $this->db->resultAll();
   }
 
   public function getPenulisById($id_penulis)
   {
+    $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id_penulis=:id_penulis');
+    $this->db->bind('id_penulis', $id_penulis);
+    return $this->db->resultSingle();
   }
 
   public function addPenulis($nama_penulis, $kewarganegaraan)
   {
+    $query = "INSERT INTO " . $this->table . " VALUES ('', :nama_penulis, :kewarganegaraan)";
+
+    $this->db->query($query);
+    $this->db->bind('nama_penulis', $nama_penulis);
+    $this->db->bind('kewarganegaraan', $kewarganegaraan);
+
+    $this->db->execute();
+    return $this->db->rowCount();
   }
 
   public function updatePenulis($id_penulis, $nama_penulis, $kewarganegaraan)
   {
+    $query = "UPDATE " . $this->table . " SET nama_penulis=:nama_penulis, kewarganegaraan=:kewarganegaraan WHERE id_penulis=:id_penulis";
+
+    $this->db->query($query);
+    $this->db->bind('id_penulis', $id_penulis);
+    $this->db->bind('nama_penulis', $nama_penulis);
+    $this->db->bind('kewarganegaraan', $kewarganegaraan);
+
+    $this->db->execute();
+    return $this->db->rowCount();
   }
 
   public function hapusPenulis($id_penulis)
   {
+    $query = "DELETE FROM " . $this->table . " WHERE id_penulis=:id_penulis";
+
+    $this->db->query($query);
+    $this->db->bind('id_penulis', $id_penulis);
+
+    $this->db->execute();
+    return $this->db->rowCount();
   }
 }
